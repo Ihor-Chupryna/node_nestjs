21a1dd60b6693ba0bdfbcc19eda7d415c8f9c2c4
Ihor-Chupryna <igchup1985@gmail.com>
2024-06-08 12:12:46 +0300

    guards, param decorator

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 
 import { RefreshTokenRepository } from '../../repository/services/refresh-token.repository';
@@ -6,6 +6,7 @@ import { UserRepository } from '../../repository/services/user.repository';
 import { UserService } from '../../user/services/user.service';
 import { SignInReqDto } from '../dto/req/sign-in.req.dto';
 import { SignUpReqDto } from '../dto/req/sign-up.req.dto';
+import { AuthResDto } from '../dto/res/auth.res.dto';
 import { AuthMapper } from './auth.mapper';
 import { AuthCacheService } from './auth-cache.service';
 import { TokenService } from './token.service';
@@ -44,7 +45,38 @@ export class AuthService {
     return await AuthMapper.toResponseDTO(user, pair);
   }
 
-  public async singIn(dto: SignInReqDto): Promise<any> {
-    return 'dook';
+  public async singIn(dto: SignInReqDto): Promise<AuthResDto> {
+    const user = await this.userRepository.findOne({
+      where: { email: dto.email },
+      select: { password: true, id: true },
+    });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    const pair = await this.tokenService.generateAuthToken({
+      userId: user.id,
+      deviceId: dto.deviceId,
+    });
+
+    await Promise.all([
+      this.refreshTokenRepository.delete({
+        deviceId: dto.deviceId,
+        user_id: user.id,
+      }),
+      this.authCacheService.deleteToken(user.id, dto.deviceId),
+    ]);
+
+    await Promise.all([
+      this.refreshTokenRepository.save(
+        this.refreshTokenRepository.create({
+          user_id: user.id,
+          refreshToken: pair.refreshToken,
+          deviceId: dto.deviceId,
+        }),
+      ),
+      this.authCacheService.saveToken(pair.accessToken, user.id, dto.deviceId),
+    ]);
+    const userEntity = await this.userRepository.findOneBy({ id: user.id });
+    return await AuthMapper.toResponseDTO(userEntity, pair);
   }
 }
